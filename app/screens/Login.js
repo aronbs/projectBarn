4583d5ea855e4b0ae546be006a94ee10c9b59697
Kieran Chadwick <kieranchadwick@rentalcars.com>
2019-03-08 15:06:20 +0000

    feat: Using stylesheet

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
-import { View, StatusBar } from "react-native";
+import { View, StatusBar, StyleSheet } from "react-native";
 import HeaderTitle from "../components/HeaderTitle";
 import Typography from "../components/Typography";
 import { SPACING } from "../styles";
+
+const styles = StyleSheet.create({
+  loginContainer: {
+    marginVertical: SPACING.LARGEST,
+    marginHorizontal: SPACING.LARGE
+  }
+});
 
 class LoginScreen extends Component {
   static navigationOptions = {
@@ -13,13 +20,7 @@ class LoginScreen extends Component {
     return (
       <View>
         <StatusBar barStyle="light-content" />
-
-        <View
-          style={{
-            marginVertical: SPACING.LARGEST,
-            marginHorizontal: SPACING.LARGE
-          }}
-        >
+        <View style={styles.loginContainer}>
           <Typography.Heading>Welcome back</Typography.Heading>
           <Typography.Body>Login to start managing your rides.</Typography.Body>
         </View>
