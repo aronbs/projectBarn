671c150bb9ba5ee0d177e972ce666e3bc74d284f
jiebinsu <jiebinsu@gmail.com>
2019-03-08 16:27:30 +0000

    feat: Setup storybook

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import StorybookUI from './app/storybook/index';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const displayApp = __DEV__ ? StorybookUI : App;
+
+AppRegistry.registerComponent(appName, () => displayApp);
