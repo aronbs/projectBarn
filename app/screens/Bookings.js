ad4fb5705054ebaaedc7bb702aebcd024b4b937e
Kieran Chadwick <kieranchadwick@rentalcars.com>
2019-03-22 15:02:21 +0000

    Grey background

--- app/screens/Bookings.js
+++ app/screens/Bookings.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import { View, StatusBar, StyleSheet } from "react-native";
 import Booking from "../components/Booking";
+import { COLOURS } from "../styles";
 
-const styles = StyleSheet.create({ container: {} });
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLOURS.GRAYSCALE_LIGHTEST,
+    flex: 1
+  }
+});
 
 class BookingsScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
-        <Booking />
+        <Booking
+          pickupTime="8:10 AM"
+          pickupLocation="Somewhere over here"
+          dropoffLocation="Somewhere over there"
+        />
       </View>
     );
   }
