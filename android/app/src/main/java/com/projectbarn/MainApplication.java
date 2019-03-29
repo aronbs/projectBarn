37b46d026c313323c47f592d556c5abb5bfb56fb
jiebinsu <jiebinsu@gmail.com>
2019-03-29 14:54:49 +0000

    feat: Add google maps for  android/ios

--- android/app/src/main/java/com/projectbarn/MainApplication.java
+++ android/app/src/main/java/com/projectbarn/MainApplication.java
@@ -1,6 +1,7 @@
 package com.projectbarn;
 
 import android.app.Application;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import com.facebook.react.ReactApplication;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage()
+          new RNGestureHandlerPackage(),
+          new MapsPackage()
       );
     }
 
